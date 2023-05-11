7480254b8eca1795e3c4a3fbce28cec4ec6a9f9e
igaimerca <igaimerca@gmail.com>
2023-05-11 15:40:07 +0200

    feat: landing page

--- src/App.js
+++ src/App.js
@@ -26,20 +26,3 @@ export default function App() {
     </>
   );
 }
-
-// export default EventLandingPage;
-// export default HotelTravelLandingPage;
-// export default AgencyLandingPage;
-// export default SaaSProductLandingPage;
-// export default RestaurantLandingPage;
-// export default ServiceLandingPage;
-// export default HostingCloudLandingPage;
-
-// export default LoginPage;
-// export default SignupPage;
-// export default PricingPage;
-// export default AboutUsPage;
-// export default ContactUsPage;
-// export default BlogIndexPage;
-// export default TermsOfServicePage;
-// export default PrivacyPolicyPage;
